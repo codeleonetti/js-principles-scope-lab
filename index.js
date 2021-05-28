b61bf5f5a39d45ac08b152b3e2c069dd09e6dfd5
john <john.d.leonetti@gmail.com>
2021-05-27 18:42:25 -0700

    commit tree ebb17ad10cefdcd8c59b6ca3eba27f424d7a25cc

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+    customerName = "BOB";
+}
+
+function setBestCustomer(){
+    setBestCustomer = "not bob"
+}
+
+function overwriteBestCustomer(){
+
+}
+
+function changeLeastFavoriteCustomer(){
+
+}
